3b972b0ea497f1e46ae8541b6783a4302b1714bc
RainSure <1114560341@qq.com>
2024-12-25 12:46:25 +0800

    feat: modify CMakeLists.txt

--- third_party/CGAL-6.0.1/CGALConfigVersion.cmake
+++ third_party/CGAL-6.0.1/CGALConfigVersion.cmake
@@ -2,4 +2,4 @@
 # This file points to the CGALConfigVersion.cmake for header-only CGAL.
 #
 
-include("/Users/bytedance/Desktop/cpp/PoissonReconCGAL/third_party/CGAL-6.0.1/lib/cmake/CGAL/CGALConfigVersion.cmake")
+include("D:/Code/cpp/PoissonReconCGAL/third_party/CGAL-6.0.1/lib/cmake/CGAL/CGALConfigVersion.cmake")
