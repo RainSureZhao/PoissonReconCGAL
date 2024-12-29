ebbd01da1da0eb81af9697888901cce00009e53e
zhaorunshuo <zhaorunshuo@bytedance.com>
2024-12-29 17:44:06 +0800

    feat: add code

--- tests/weighted_points_test.cpp
+++ tests/weighted_points_test.cpp
@@ -9,8 +9,19 @@ TEST(WeightPointTest, Test1) {
         R"(../data/weighted_points_corners.xyz)",
         R"(../data/weighted_points_edges.xyz)",
         R"(../data/refine_balls.xyz)",
-        R"(../data/weighted_points_output.xyz)"
+        R"(../data/weighted_points_layerblock_output.xyz)"
     );
-    service::weighted_points::Convert_weighted_points_to_mesh_separately(R"(../data/weighted_points_output.xyz)",
-        R"(../data/weighted_points/weight_point)");
+    service::weighted_points::Convert_weighted_points_to_mesh_separately(R"(../data/weighted_points_layerblock_output.xyz)",
+        R"(../data/weighted_points_layerblock/weight_point)");
+}
+
+TEST(WeightPointTest, Test2) {
+    service::weighted_points::Refine_weighted_points(
+        R"(../data/weighted_points_corners.xyz)",
+        R"(../data/weighted_points_edges.xyz)",
+        R"(../data/refine_balls.xyz)",
+        R"(../data/weighted_points_layerblock_output.xyz)"
+    );
+    service::weighted_points::Convert_weighted_points_to_mesh(R"(../data/weighted_points_layerblock_output.xyz)",
+        R"(../data/weighted_points_layerblock/weight_points.off)");
 }
